62f4a0b3d019d455631240cbab2d192df80c78fb
Atomowyy <wojciech.lewkoo@gmail.com>
2023-11-25 17:29:11 +0100

    vidual correction

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import mysql.connector
 import os
 import hashlib
+import time
 from mysql.connector import Error
 from dotenv import load_dotenv
 from selenium import webdriver
@@ -17,11 +18,10 @@ def connector():
                                              port=os.getenv('port'),
                                              user=os.getenv('user'),
                                              password=os.getenv('password'))
-
-        #Uncomment below to check if database is connected
+        # Uncomment below to check if database is connected
         """
         if connection.is_connected():
-            print("Connected succesfully")
+            print("Connected successfully")
         """
     except Error as e:
         print("Error while connecting to MySQL", e)
@@ -30,6 +30,7 @@ def connector():
 
 
 def login():
+    print("\n" * 100)
     user_login = input("Login: ")
     user_password = input("Password:")
     verification_hash = hashlib.new("SHA256")
@@ -41,20 +42,23 @@ def login():
         cursor.execute(querry)
         id = cursor.fetchone()
         if id == None:
-            print("There is no user called: "+user_login+"\nPlease register first!")
+            print("There is no user called: " + user_login + "\nPlease register first!")
+            time.sleep(2)
+            print("\n" * 100)
             opening_screen()
         else:
             querry = """SELECT password FROM users WHERE username = '%s'""" % (user_login)
             cursor.execute(querry)
             hashed_password = cursor.fetchone()
             if verification_hash.hexdigest() == hashed_password[0]:
-                print("Succesfully logged in!")
+                print("Successfully logged in!")
             else:
                 print("Your password is incorrect!")
                 opening_screen()
     except Error as e:
         print("Error", e)
     finally:
+        time.sleep(3)
         if connected_database.is_connected():
             cursor.close()
             connected_database.close()
@@ -64,10 +68,12 @@ def login():
 
 
 def register():
+    print("\n" * 100)
     new_user_login = input("Enter your login: ")
     new_user_password = input("Enter your password:")
     confirm_password = input("Enter password again:")
     while new_user_password != confirm_password:
+        print("\n" * 100)
         print("Passwords doesn't match! Try again:")
         new_user_password = input("Enter your password:")
         confirm_password = input("Enter password again:")
@@ -76,10 +82,12 @@ def register():
     try:
         connected_database = connector()
         cursor = connected_database.cursor()
-        querry = """INSERT INTO users (username, password) VALUES('%s','%s')""" % (new_user_login, hashed_new_password.hexdigest())
+        querry = """INSERT INTO users (username, password) VALUES('%s','%s')""" % (
+        new_user_login, hashed_new_password.hexdigest())
         cursor.execute(querry)
         connected_database.commit()
-        print("User registered succesfully!")
+        print("User registered successfully!")
+        time.sleep(3)
     except Error as e:
         print("Error", e)
     finally:
@@ -90,9 +98,13 @@ def register():
 
 
 def opening_screen():
-    print("----------------------------------------\nWelcome to LEGO Investor - price checker\n----------------------------------------\n1.Login\n2.Create new account\n3.Exit\n")
+    print("\n" * 100)
+    print(
+        "----------------------------------------\nWelcome to LEGO Investor - price "
+        "checker\n----------------------------------------\n1.Login\n2.Create new account\n3.Exit\n")
     decision = input()
-    while decision.isnumeric() == False or int(decision) != 1 and int(decision) != 2 and int(decision) != 3:
+    while decision.isnumeric() == False or int(decision) not in [1, 2, 3]:
+        print("\n" * 100)
         print("It looks like that you have entered wrong option. Try again!")
         decision = input("Choose number between: 1(login), 2(register), 3(exit)")
     if int(decision) == 1:
@@ -105,18 +117,21 @@ def opening_screen():
 
 
 def add_set(user_id, username):
+    print("\n" * 100)
     set_number = int(input("Enter your set number: "))
     price = float(input("Enter price that you've paid: "))
     try:
         connected_database = connector()
         cursor = connected_database.cursor()
-        querry = """INSERT INTO sets (user_id, set_number, bought_price) VALUES('%s','%s','%s')""" % (user_id, set_number, price)
+        querry = """INSERT INTO sets (user_id, set_number, bought_price) VALUES('%s','%s','%s')""" % (
+        user_id, set_number, price)
         cursor.execute(querry)
         connected_database.commit()
         print("LEGO set added!")
     except Error as e:
         print("Something went wrong!", e)
     finally:
+        time.sleep(3)
         if connected_database.is_connected():
             cursor.close()
             connected_database.close()
@@ -124,6 +139,7 @@ def add_set(user_id, username):
 
 
 def check_set_price(user_id, username):
+    print("\n" * 100)
     scrapping_info = []
     try:
         connected_database = connector()
@@ -131,21 +147,24 @@ def check_set_price(user_id, username):
         querry = """SELECT set_number, bought_price FROM sets WHERE user_id = '%s'""" % (user_id)
         cursor.execute(querry)
         sets = cursor.fetchall()
-        print("LEGO set number | Price that you've paid | New price | Used price")
         for i in sets:
-            row = [i[0],i[1]]
+            row = [i[0], i[1]]
             scrapping_info.append(row)
         complete_informations = bricklink_scrapping(scrapping_info)
         display_table(complete_informations, "price_table")
     except Error as e:
         print("Something went wrong!", e)
     finally:
+        time.sleep(3)
+        continuation = input("do you want to continue?")
         if connected_database.is_connected():
             connected_database.close()
             cursor.close()
         logged_in_menu(user_id, username)
 
+
 def list_sets(user_id, username):
+    print("\n" * 100)
     try:
         connected_database = connector()
         cursor = connected_database.cursor()
@@ -156,6 +175,8 @@ def list_sets(user_id, username):
     except Error as e:
         print("Something went wrong!", e)
     finally:
+        time.sleep(3)
+        continuation = input("Do you want to continue?")
         if connected_database.is_connected():
             connected_database.close()
             cursor.close()
@@ -163,12 +184,18 @@ def list_sets(user_id, username):
 
 
 def logged_in_menu(user_id, username):
-    print("----------------------------------------\nWelcome, "+username+"!\n----------------------------------------")
-    print("1.Add new LEGO set\n2.Show LEGO sets that you already own\n3.Check prices for your LEGO sets online\n4.Logout")
+    print("\n" * 100)
+    print(
+        "----------------------------------------\nWelcome, " + username + "!\n----------------------------------------")
+    print(
+        "1.Add new LEGO set\n2.Show LEGO sets that you already own\n3.Check prices for your LEGO sets online\n4.Logout")
     decision = input()
-    while decision.isnumeric() == False or int(decision) != 1 and int(decision) != 2 and int(decision) != 3 and int(decision) != 4:
+    while decision.isnumeric() == False or int(decision) != 1 and int(decision) != 2 and int(decision) != 3 and int(
+            decision) != 4:
+        print("\n" * 100)
         print("It looks like that you have entered wrong option. Try again!")
-        decision = input()
+        decision = input("1.Add new LEGO set\n2.Show LEGO sets that you already own\n3.Check prices for your LEGO "
+                         "sets online\n4.Logout")
     if int(decision) == 1:
         add_set(user_id, username)
     elif int(decision) == 2:
@@ -183,24 +210,27 @@ def bricklink_scrapping(set_info):
     driver = webdriver.Chrome()
     for i in set_info:
         set_id = i[0]
-        driver.get("https://www.bricklink.com/v2/catalog/catalogitem.page?S="+str(set_id)+"#T=P")
-        new_medium_price = driver.find_element(By.XPATH, '//*[@id="_idPGContents"]/table/tbody/tr[3]/td[3]/table/tbody/tr[4]/td[2]/b')
+        driver.get("https://www.bricklink.com/v2/catalog/catalogitem.page?S=" + str(set_id) + "#T=P")
+        new_medium_price = driver.find_element(By.XPATH, '//*[@id="_idPGContents"]/table/tbody/tr[3]/td['
+                                                         '3]/table/tbody/tr[4]/td[2]/b')
         i.append(new_medium_price.text)
-        used_medium_price = driver.find_element(By.XPATH, '//*[@id="_idPGContents"]/table/tbody/tr[3]/td[4]/table/tbody/tr[4]/td[2]/b')
+        used_medium_price = driver.find_element(By.XPATH, '//*[@id="_idPGContents"]/table/tbody/tr[3]/td['
+                                                          '4]/table/tbody/tr[4]/td[2]/b')
         i.append(used_medium_price.text)
     driver.close()
     return set_info
 
 
 def display_table(price_table, type):
-    if type=="display_table":
+    print("\n" * 100)
+    if type == "display_table":
         prepared_table = []
-        prepared_table.append(["LEGO set number","Price that you've paid"])
-        for i in  price_table:
-            row = [str(i[0]),"PLN "+str(i[1])]
+        prepared_table.append(["LEGO set number", "Price that you've paid"])
+        for i in price_table:
+            row = [str(i[0]), "PLN " + str(i[1])]
             prepared_table.append(row)
         print(tabulate(prepared_table, headers="firstrow", tablefmt="simple_outline"))
-    elif type=="price_table":
+    elif type == "price_table":
         prepared_table = []
         prepared_table.append(["LEGO set number", "Price that you've paid", "New set price", "Used set price"])
         for i in price_table:
@@ -211,6 +241,5 @@ def display_table(price_table, type):
         print("Something went wrong!")
 
 
-
 while True:
-   opening_screen()
+    opening_screen()
